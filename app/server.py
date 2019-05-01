defd69d48beefe6ae228f546125525c6c18bd941
Oliver Britton <oliver.britton@cs.ox.ac.uk>
2019-05-01 20:49:13 +0100

    Added resnet50 learner v1

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from fastai import *
 from fastai.vision import *
 
 # export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://www.dropbox.com/s/8a4vllikvwamsrb/heresy_v1.pkl?dl=1'
+export_file_name = 'heresy_v1.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['chaos', 'imperial'] 
 path = Path(__file__).parent
 
 app = Starlette()
